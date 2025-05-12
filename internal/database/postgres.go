6abf3ece3b6ae9f452ccc110df054d90ba0b2d20
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-12 20:32:40 +0530

    add database conn

---
+++ internal/database/postgres.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+func NewDatabaseConn(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string, env string) (*sql.DB, error) {
+	if len(addr) == 0 {
+		return nil, errors.New("[POSTGRESQL] DB connection address is empty")
+	}
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
