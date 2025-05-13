d624896ee5112e6c42ea045ff4b0802bea763e5c
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-14 04:54:24 +0530

    add v1 handler route

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,9 +3,10 @@ package handlers
 import (
 	"net/http"
 
+	v1hdr "github.com/soumayg9673/inshorts-assessment/internal/handlers/v1"
 	"github.com/soumayg9673/inshorts-assessment/internal/service"
 )
 
 func RegisterRoutes(mux *http.ServeMux, s service.Service) {
-
+	v1hdr.NewV1Handlers(mux, s.V1)
 }
