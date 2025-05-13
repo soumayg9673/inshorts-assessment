c2f57a66c655b11d48cd42f79c0821b8f2b042b4
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-14 04:54:36 +0530

    sample template for category

--- internal/handlers/v1/news.go
+++ internal/handlers/v1/news.go
@@ -2,8 +2,14 @@ package v1hdr
 
 import "net/http"
 
-func (h V1Hdr) news() {
+func (h *V1Hdr) news() {
 	mux := http.NewServeMux()
-	// TODO: Register API routes
+
+	mux.HandleFunc("GET /category", h.GetNewsByCategory)
+
 	h.MUX.Handle("/news/", http.StripPrefix("/news", mux))
+}
+
+func (h *V1Hdr) GetNewsByCategory(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 }
