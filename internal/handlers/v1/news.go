bcbe6964a7a88d15df33cdd000a87b33021e96b6
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-14 22:46:12 +0530

    handle errors for bad request and internal errors

--- internal/handlers/v1/news.go
+++ internal/handlers/v1/news.go
@@ -2,6 +2,9 @@ package v1hdr
 
 import (
 	"net/http"
+
+	"github.com/soumayg9673/inshorts-assessment/cmd/api/errors"
+	cjson "github.com/soumayg9673/inshorts-assessment/cmd/api/json"
 )
 
 func (h *V1Hdr) news() {
@@ -16,11 +19,19 @@ func (h *V1Hdr) GetNewsByCategory(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	/*
 		Validate key "cat" in query parameters
-		Return statusCode = 400 BAD REQUEST (if "cat" not found in query parameters)
+		Return statusCode = 400 BAD REQUEST
+		1. if "cat" not found in query parameters
+		2. Key "cat" have no value
 	*/
-	if _, ok := q["cat"]; !ok {
-		w.WriteHeader(400)
+	if v, ok := q["cat"]; len(v) == 0 || !ok {
+		cjson.WriteJSONError(w, errors.BadRequst())
 		return
 	}
-	w.WriteHeader(200)
+
+	data, err := h.SVC.GetNewsByCategory(q["cat"])
+	if err != nil {
+		cjson.WriteJSONError(w, errors.SomethingWentWrong())
+		return
+	}
+	cjson.WriteJSON(w, 200, data)
 }
