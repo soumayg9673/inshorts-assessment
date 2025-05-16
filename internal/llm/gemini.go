ca748a705a5abb34ac520448df037dec07fd38dd
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-16 21:40:46 +0530

    gemini get by text

--- internal/llm/gemini.go
+++ internal/llm/gemini.go
@@ -2,7 +2,7 @@ package llm
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
 
 	"go.uber.org/zap"
@@ -14,8 +14,7 @@ type GeminiAi struct {
 	Client *genai.Client
 }
 
-func newGeminiAi(log *zap.Logger) GeminiAi {
-	ctx := context.Background()
+func newGeminiAi(log *zap.Logger, ctx context.Context) GeminiAi {
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{
 		APIKey:  os.Getenv("GEMINI_API_KEY"),
 		Backend: genai.BackendGeminiAPI,
@@ -24,23 +23,27 @@ func newGeminiAi(log *zap.Logger) GeminiAi {
 		log.Debug(err.Error())
 	}
 
+	log.Info("Gemini AI connected!")
 	return GeminiAi{
 		Ctx:    ctx,
 		Client: client,
 	}
 }
 
-func (g GeminiAi) QueryText(gm string, url string) {
+func (g GeminiAi) QueryText(gm string, url string) string {
 	config := &genai.GenerateContentConfig{
 		SystemInstruction: genai.NewContentFromText("You are a user interested in news article summary without clicking on the link", genai.RoleUser),
 	}
 
-	result, _ := g.Client.Models.GenerateContent(
+	result, err := g.Client.Models.GenerateContent(
 		g.Ctx,
 		gm,
 		genai.Text(url),
 		config,
 	)
+	if err != nil {
+		log.Println(err.Error())
+	}
 
-	fmt.Println(result.Text())
+	return result.Text()
 }
