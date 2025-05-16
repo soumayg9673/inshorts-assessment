22f2aea9a1c3d2293ee468ac3c069ad31ee2dbf0
soumayg9673 <soumayg9673@palanhaar.in>
2025-05-16 21:37:54 +0530

    update get llm summary and add llm summary to db table

--- internal/repository/v1/news.go
+++ internal/repository/v1/news.go
@@ -17,6 +17,7 @@ func (rp *V1Rpo) GetNewsByCategory(q []string) ([]newsv1.NewsSql, error) {
 	for rows.Next() {
 		itr := newsv1.NewsSql{}
 		if err := rows.Scan(
+			&itr.Id,
 			&itr.Title,
 			&itr.Description,
 			&itr.Url,
@@ -24,6 +25,7 @@ func (rp *V1Rpo) GetNewsByCategory(q []string) ([]newsv1.NewsSql, error) {
 			&itr.Source,
 			&itr.Category,
 			&itr.RevScore,
+			&itr.LlmSummary,
 			&itr.Latitude,
 			&itr.Longitude,
 		); err != nil {
@@ -49,4 +51,14 @@ func (rp *V1Rpo) GetNewsBySource() {
 
 func (rp *V1Rpo) GetNewsByNearby() {
 
+}
+
+func (rp *V1Rpo) PatchLlmSummary(id, ls string) {
+	var t string
+	row := rp.DB.PatchLlmSummary(id, ls)
+	if err := row.Scan(&t); err != nil {
+		rp.LOG.Debug(err.Error(),
+			zap.String("env", rp.ENV),
+		)
+	}
 }
